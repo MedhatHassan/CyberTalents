3ccd71d958c932b99ff7a33015c90ae0daa25881
m_1337 <eng.medhathassanali@gmail.com>
2024-06-17 23:23:30 -0400

    Update: Mon Jun 17 11:23:30 PM EDT 2024

---
+++ Web/MaximumCourage/dumped files/flag.php
@@ -0,0 +1,7 @@
+You can't view this flag directly.
+<!-- PHP source doesn't appear on HTML comments -->
+<?php
+exit();
+die();
+$secret_key = 'be607453caada6a05d00c0ea0057f733';
+?>
---
+++ Web/MaximumCourage/dumped files/index.php
@@ -0,0 +1,7 @@
+<title>MAX</title>
+<body style="margin:0px;padding:0px;background-color:#000;color:#01ba17">
+<div style="padding:100px;width:70%;margin: 0px auto;font-size:26px;">
+Welcome to max's hideway.<br /><br />
+I learn security by watching hackers break my website, so to my understanding the PHP source code is never served to the user so you can't see the content of my <a style="color:#01ba17" href="flag.php" target="_BLANK" />flag.php</a>
+</div>
+</body>
